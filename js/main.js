d5ca9bb67233dfd40f4dbccbe86d15dee9665b3a
darshanakkhichi <darshanakkhichi@gmail.com>
2019-01-13 15:46:54 +0530

    update 8

--- js/main.js
+++ js/main.js
@@ -128,4 +128,34 @@ $(document).ready(function(){
           $('#mc_embed_signup').find('form').ajaxChimp();
       });   
 
+
+      // var nodemailer = require('nodemailer');
+
+     
+      // function sendMail(){
+      //   var transporter = nodemailer.createTransport({
+      //       service: 'gmail',
+      //       auth: {
+      //         user: 'youremail@gmail.com',
+      //         pass: 'yourpassword'
+      //       }
+      //     });
+          
+      //     var mailOptions = {
+      //       from: 'youremail@gmail.com',
+      //       to: 'darshanakkhichi@gmail.com',
+      //       subject: 'Sending Email using Node.js',
+      //       text: 'That was easy!'
+      //     };
+          
+      //     transporter.sendMail(mailOptions, function(error, info){
+      //       if (error) {
+      //         console.log(error);
+      //       } else {
+      //         console.log('Email sent: ' + info.response);
+      //       }
+      //     });
+      // }
  });
+ 
+ 
